c76c7580be219a45b145bad2ce2f2974d1a27aa1
Sido van Gennip <sido@angelmechanics.com>
2011-09-07 14:08:25 +0200

    First page commit

---
+++ cache/59/16/0b81b59510875e40be54a122b496.php
@@ -0,0 +1,28 @@
+<?php
+
+/* index.html.twig */
+class __TwigTemplate_59160b81b59510875e40be54a122b496 extends Twig_Template
+{
+    protected function doGetParent(array $context)
+    {
+        return false;
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $context = array_merge($this->env->getGlobals(), $context);
+
+        // line 1
+        echo "This is Vine! (twig)";
+    }
+
+    public function getTemplateName()
+    {
+        return "index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+}
